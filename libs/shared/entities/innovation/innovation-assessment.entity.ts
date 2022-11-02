69ab343ddac9812fa1a24ea9a6dd900ee0ea4c6f
Antonio Simoes <antoniosimoes@Antonios-MBP.Home>
2022-11-02 14:24:25 +0000

    cleans up

--- libs/shared/entities/innovation/innovation-assessment.entity.ts
+++ libs/shared/entities/innovation/innovation-assessment.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 import { BaseEntity } from '../base.entity';
 
