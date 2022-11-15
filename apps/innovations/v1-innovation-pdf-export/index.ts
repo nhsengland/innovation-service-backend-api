7385c41b3770a51c7487a0ad2ff9b58bbd379d8d
Antonio Simoes <antonio.simoes@bjss.com>
2022-11-15 12:17:11 +0000

    adds styling refactor

--- apps/innovations/v1-innovation-pdf-export/index.ts
+++ apps/innovations/v1-innovation-pdf-export/index.ts
@@ -11,6 +11,7 @@ import { type BodyType, BodySchema, ParamsType, ParamsSchema } from './validatio
 import type { InnovationExportSectionAnswerType, InnovationExportSectionItemType, InnovationExportSectionType } from '../_types/innovation.types';
 import { container } from '../_config';
 import { InnovationsServiceSymbol, InnovationsServiceType } from '../_services/interfaces';
+import { buildDocumentHeaderDefinition, buildDocumentFooterDefinition, buildDocumentTOCDefinition, buildDocumentStylesDefinition } from '../_helpers/innovation.pdf.styles'
 class PostInnovationPDFExport {
 
  
@@ -26,90 +27,18 @@ class PostInnovationPDFExport {
       const innovation = await innovationService.getInnovationInfo(params.innovationId, {});
 
       const documentDefinition = {
-        header: (current: number) => {
-          if (current > 1) {
-            return {
-              columns: [{
-                text: 'Innovation Record',
-                style: 'dimmed',
-                margin: [10, 10, 10, 10],
-                alignment: 'right',
-                italic: true,
-              }],
-            };
-          } else return;
-        },
-        footer: (current: number, total: number) => {
-          if (current > 1) {
-            return {
-              columns: [
-                {
-                  text: `${current} of ${total}`,
-                  style: 'footer',
-                  alignment: 'right',
-                  margin: [0, 0, 10, 10],
-                },
-              ]
-            };
-          } else return;
-        },
-        content: [
-          {
-            text: innovation.name,
-            style: 'documentTitle',
-            pageBreak: 'after',
-          },
-          { 
-            toc: {
-              title: { text: 'INDEX', style: 'header' },
-              numberStyle: { bold: true }
-            }
-          },
-        ],
-        styles: {
-          dimmed: {
-            fontSize: 10,
-            color: '#999999',
-            italic: true,
-          },
-          documentTitle: {
-            fontSize: 26,
-            bold: true,
-            alignment: 'center',
-            margin: [0, 0, 0, 20],
-          },
-          header: {
-            fontSize: 20,
-            bold: true
-          },
-          subheader: {
-            fontSize: 15,
-            bold: true
-          },
-          quote: {
-            italics: true
-          },
-          small: {
-            fontSize: 10
-          },
-          question: {
-            fontSize: 12,
-            bold: true,
-            italics: true,
-          },
-          footer: {
-            fontSize: 10,
-            color: '#999999',
-            italics: true,           
-          },
-        }
+        header: buildDocumentHeaderDefinition(),
+        footer: buildDocumentFooterDefinition(),
+        content: buildDocumentTOCDefinition(innovation.name),
+        styles: buildDocumentStylesDefinition(),
       }
 
+      let sectionNumber = 1;
       body.forEach((entry: {title: string, sections: InnovationExportSectionType[]}) => {
 
         documentDefinition.content.push({
-          text: entry.title || '',
-          style: 'header',
+          text: `${sectionNumber}. ${ entry.title }`,
+          style: 'sectionTitle',
           tocItem: true,
           tocStyle: { italics: true },
           tocMargin: [0, 10, 0, 0],
@@ -117,18 +46,21 @@ class PostInnovationPDFExport {
           pageBreak: 'before',
         } as any);
 
+        let subSectionNumber = 1;
+
         entry.sections.forEach((section: InnovationExportSectionItemType ) => {
           documentDefinition.content.push({
-            text: section.section as any,
+            text: `${sectionNumber}.${subSectionNumber} ${section.section}`,
             style: 'subheader',
-            margin: [0, 10],
+            margin: [5, 20],
           } as any);
 
+          let questionNumber = 1;
           section.answers.forEach((answer: InnovationExportSectionAnswerType) => {
             documentDefinition.content.push({
-              text: answer.label,
+              text: `${sectionNumber}.${subSectionNumber}.${questionNumber} ${answer.label}`,
               style: 'question',
-              margin: [0, 5],
+              margin: [10, 10,],
             } as any);
 
             const answers = answer.value.split('\n');
@@ -136,19 +68,22 @@ class PostInnovationPDFExport {
             if (answers.length > 1) {
               documentDefinition.content.push({
                 ul: answers,
-                style: 'small',
-                margin: [5, 2],
+                style: 'answer',
+                margin: [15, 2],
               } as any);
             }  else {
               documentDefinition.content.push({
                 text: answers[0] === 'undefined' ? '-' : answers[0] === '' ? '-' : answers[0],
-                style: 'small',
-                margin: [5, 2],
+                style: 'answer',
+                margin: [15, 2],
               } as any);
             }
 
+            questionNumber++;
           });
+          subSectionNumber++;
         });
+        sectionNumber++;
        });
 
       
