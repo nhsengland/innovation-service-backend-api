6ec8111774fe5384510807fa4137d6d50fc378f2
Katia <Katia.Duarte@bjss.com>
2022-11-23 14:41:56 +0000

    add service

--- apps/users/_services/statistics.service.ts
+++ apps/users/_services/statistics.service.ts
@@ -32,12 +32,33 @@ export class StatisticsService  extends BaseService {
 
 
  async assignedInnovations(userId: string): Promise<{count: number; total: number; overdue: number}> {
-  console.log(userId);
+
+  const count = await this.sqlConnection.createQueryBuilder(InnovationEntity, 'innovation')
+      .leftJoinAndSelect('innovation.assessments', 'assessments')
+      .leftJoinAndSelect('assessments.assignTo', 'assignTo')
+      .where('innovation.status IN (:...assessmentInnovationStatus)', { assessmentInnovationStatus: [InnovationStatusEnum.NEEDS_ASSESSMENT] })
+      .andWhere('assignTo.id = :userId', { userId })
+      .getCount();
+
+    const total = await this.sqlConnection.createQueryBuilder(InnovationEntity, 'innovation')
+      .leftJoinAndSelect('innovation.assessments', 'assessments')
+      .leftJoinAndSelect('assessments.assignTo', 'assignTo')
+      .where('innovation.status IN (:...assessmentInnovationStatus)', { assessmentInnovationStatus: [InnovationStatusEnum.NEEDS_ASSESSMENT] })
+
+      .getCount();
+
+    const overdueCount = await this.sqlConnection.createQueryBuilder(InnovationEntity, 'innovation')
+      .leftJoinAndSelect('innovation.assessments', 'assessments')
+      .leftJoinAndSelect('assessments.assignTo', 'assignTo')
+      .where('innovation.status IN (:...assessmentInnovationStatus)', { assessmentInnovationStatus: [InnovationStatusEnum.NEEDS_ASSESSMENT] })
+      .andWhere(`DATEDIFF(day, innovation.submitted_at, GETDATE()) > 7 AND assessments.finished_at IS NULL`)
+      .getCount();
+
 
   return {
-    count: 0,
-    total: 0,
-    overdue: 0
+    count: count,
+    total: total,
+    overdue: overdueCount
   }
  }
 }
