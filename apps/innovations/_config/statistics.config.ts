75c65674375197f7f2f3c8cce6d1becd6132fb4c
Antonio Simoes <antonio.simoes@bjss.com>
2022-11-24 10:40:33 +0000

    adds statistics to NA innovation overview

--- apps/innovations/_config/statistics.config.ts
+++ apps/innovations/_config/statistics.config.ts
@@ -2,8 +2,10 @@ import type { DateISOType } from '@innovations/shared/types'
 import { InnovationStatisticsEnum } from '../_enums/innovation.enums'
 import { actionsToReviewStatisticsHandler } from '../_handlers/statistics/actions-to-review.handler'
 import { actionsToSubmitStatisticsHandler } from '../_handlers/statistics/actions-to-submit.handler'
+import { sectionsSubmittedSinceAssessmentStartStatisticsHandler } from '../_handlers/statistics/sections-submitted-since-assessment-start.handler'
 import { sectionsSubmittedSinceSupportStartStatisticsHandler } from '../_handlers/statistics/sections-submitted-since-support-start.handler'
 import { sectionsSubmittedStatisticsHandler } from '../_handlers/statistics/sections-submitted.handler'
+import { unreadMessagesThreadsInitiatedByStatisticsHandler } from '../_handlers/statistics/unread-messages-initiated-by-na.handler'
 import { unreadMessagesStatisticsHandler } from '../_handlers/statistics/unread-messages.handler'
 
 export const INNOVATION_STATISTICS_CONFIG: Record<keyof typeof InnovationStatisticsEnum, {
@@ -24,6 +26,12 @@ export const INNOVATION_STATISTICS_CONFIG: Record<keyof typeof InnovationStatist
   [InnovationStatisticsEnum.SECTIONS_SUBMITTED_SINCE_SUPPORT_START_COUNTER]: {
     handler: sectionsSubmittedSinceSupportStartStatisticsHandler,
   },
+  [InnovationStatisticsEnum.SECTIONS_SUBMITTED_SINCE_ASSESSMENT_START_COUNTER]: {
+    handler: sectionsSubmittedSinceAssessmentStartStatisticsHandler,
+  },
+  [InnovationStatisticsEnum.UNREAD_MESSAGES_THREADS_INITIATED_BY_COUNTER]: {
+    handler: unreadMessagesThreadsInitiatedByStatisticsHandler,
+  },
 }
 
 export type InnovationStatisticsTemplateType = {
@@ -32,4 +40,7 @@ export type InnovationStatisticsTemplateType = {
   [InnovationStatisticsEnum.UNREAD_MESSAGES_COUNTER]: { count: number; lastSubmittedAt: null | DateISOType;},
   [InnovationStatisticsEnum.ACTIONS_TO_REVIEW_COUNTER]: { count: number; lastSubmittedAt: null | DateISOType;},
   [InnovationStatisticsEnum.SECTIONS_SUBMITTED_SINCE_SUPPORT_START_COUNTER]: { count: number; lastSubmittedAt: null | DateISOType;},
+  
+  [InnovationStatisticsEnum.SECTIONS_SUBMITTED_SINCE_ASSESSMENT_START_COUNTER]: { count: number; lastSubmittedAt: null | DateISOType;},
+  [InnovationStatisticsEnum.UNREAD_MESSAGES_THREADS_INITIATED_BY_COUNTER]: { count: number; lastSubmittedAt: null | DateISOType;},
 }
