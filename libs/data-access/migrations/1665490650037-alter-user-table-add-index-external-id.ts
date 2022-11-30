6020cc2055e860665ff48c9a6fb393b21483d0be
Manuel Sousa <manuel.sousa@bjss.com>
2022-11-30 14:49:52 +0000

    Merged PR 14620: sync database migrations from legacy API

---
+++ libs/data-access/migrations/1665490650037-alter-user-table-add-index-external-id.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class alterUserTableAddIndexExternalIdV1665490650037
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "idx_user_external_id" ON "user" ("external_id")`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "idx_user_external_id" ON "user"`);
+  }
+}
