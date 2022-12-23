41d6c18457387c0c5fc125875b6ab32096f173af
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2022-12-23 17:32:21 +0000

    create admin

--- apps/admin/_services/users.service.ts
+++ apps/admin/_services/users.service.ts
@@ -1,7 +1,10 @@
-import { UserEntity } from '@admin/shared/entities';
-import { NotFoundError, UserErrorsEnum } from '@admin/shared/errors';
+import { OrganisationEntity, OrganisationUnitEntity, RoleEntity, UserEntity, UserRoleEntity } from '@admin/shared/entities';
+import { AccessorOrganisationRoleEnum, ServiceRoleEnum, UserTypeEnum } from '@admin/shared/enums';
+import { BadRequestError, NotFoundError, OrganisationErrorsEnum, UnprocessableEntityError, UserErrorsEnum } from '@admin/shared/errors';
 import { CacheServiceSymbol, IdentityProviderService, IdentityProviderServiceSymbol } from '@admin/shared/services';
 import { CacheConfigType, CacheService } from '@admin/shared/services/storage/cache.service';
+import { userAgentPolicy } from '@azure/core-http';
+import { throws } from 'assert';
 import { inject, injectable } from 'inversify';
 import { BaseService } from './base.service';
 
@@ -45,6 +48,96 @@ export class UsersService extends BaseService {
 
       // Remove cache entry
       await this.cache.delete(user.identityId)
+    })
+  }
+
+  async createUser(
+    requestUser: { id: string },
+    data: {
+      name: string,
+      email: string,
+      type: UserTypeEnum,
+      role?: AccessorOrganisationRoleEnum
+      organisation?: {
+        acronym: string,
+        unitAcronym: string
+      }
+    }
+  ): Promise<{ id: string }> {
+
+    if (data.type === UserTypeEnum.ACCESSOR &&
+      (!data.role || !data.organisation)) {
+      throw new BadRequestError(UserErrorsEnum.USER_INVALID_ACCESSOR_PARAMETERS)
+    }
+
+    data.email = data.email.toLowerCase()
+    const password = Math.random().toString(36).slice(2) + '0aA!'
+
+    if (data.organisation) {
+      const organisation = await this.sqlConnection
+        .createQueryBuilder(OrganisationEntity, 'organisation')
+        .where('organisation.acronym = :acronym', { acronym: data.organisation.acronym })
+        .getOne()
+
+      if (!organisation) {
+        throw new NotFoundError(OrganisationErrorsEnum.ORGANISATION_NOT_FOUND)
+      }
+
+      const unit = await this.sqlConnection
+        .createQueryBuilder(OrganisationUnitEntity, 'org_unit')
+        .innerJoin('org_unit.organisation', 'org')
+        .where('org.id = :orgId', { orgId: organisation.id })
+        .andWhere('unit.acronym = :acronym', { acronym: data.organisation.unitAcronym })
+        .getOne()
+
+      if (!unit) {
+        throw new NotFoundError(OrganisationErrorsEnum.ORGANISATION_UNIT_NOT_FOUND)
+      }
+    }
+
+    let identityId: string;
+    const b2cUser = await this.identityProviderService.getUserInfoByEmail(data.email)
+
+
+    if (b2cUser) {
+      identityId = b2cUser.identityId
+
+      // user exists in b2c, check if it also exists in DB
+      const user = await this.sqlConnection
+        .createQueryBuilder(UserEntity, 'user')
+        .where('user.identityId = :identityId', { identityId: b2cUser.identityId })
+        .getOne()
+
+      if (user) {
+        throw new UnprocessableEntityError(UserErrorsEnum.USER_ALREADY_EXISTS)
+      }
+    } else {
+      // b2c user doesn't exist, create it
+      const iId = await this.identityProviderService
+        .createUser({ name: data.name, email: data.email, password: password })
+
+      identityId = iId
+    }
+
+    return await this.sqlConnection.transaction(async transaction => {
+
+      const user = await transaction.save(UserEntity, UserEntity.new({
+        identityId: identityId,
+        type: data.type,
+        createdBy: requestUser.id,
+        updatedBy: requestUser.id
+      }))
+
+
+      if (user.type === UserTypeEnum.ADMIN) {
+        const role = await transaction.createQueryBuilder(RoleEntity, 'role')
+          .where('role.name = :adminRole', { adminRole: ServiceRoleEnum.ADMIN })
+          .getOneOrFail()
+
+        await transaction.save(UserRoleEntity, UserRoleEntity.new({ user, role }))
+      }
+
+      return { id: user.id }
     })
   }
 
