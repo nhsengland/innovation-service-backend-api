5b0f3e4d9467fef8391ca6c0268bec8d4eb8fb0b
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2022-12-23 10:22:27 +0000

    formatting

--- apps/admin/_services/users.service.ts
+++ apps/admin/_services/users.service.ts
@@ -23,29 +23,29 @@ export class UsersService extends BaseService {
    * @param data partial user update options (currently only supports accountEnabled)
    *   - accountEnabled: enable or disable the user
    */
-  async updateUser(userId: string, data: {accountEnabled?: boolean | null}): Promise<void> {
-      await this.sqlConnection.transaction(async transaction => {
-        const user = await this.sqlConnection
-          .createQueryBuilder(UserEntity, 'user')
-          .where('user.id = :userId', { userId })
-          .getOne()
+  async updateUser(userId: string, data: { accountEnabled?: boolean | null }): Promise<void> {
+    await this.sqlConnection.transaction(async transaction => {
+      const user = await this.sqlConnection
+        .createQueryBuilder(UserEntity, 'user')
+        .where('user.id = :userId', { userId })
+        .getOne()
 
-        if (!user) {
-          throw new NotFoundError(UserErrorsEnum.USER_SQL_NOT_FOUND)
-        }
-        
-        await transaction.update(UserEntity, { id: userId }, {
-          ...data.accountEnabled !=null && {lockedAt: data.accountEnabled === false ? new Date().toISOString() : null}
-        })
+      if (!user) {
+        throw new NotFoundError(UserErrorsEnum.USER_SQL_NOT_FOUND)
+      }
 
-        // Update identity provider if needed
-        if (data.accountEnabled !=null) {
-          await this.identityProviderService.updateUserAsync(user.identityId, { accountEnabled: data.accountEnabled !== false })
-        }
-
-        // Remove cache entry
-        await this.cache.delete(user.identityId)
+      await transaction.update(UserEntity, { id: userId }, {
+        ...data.accountEnabled != null && { lockedAt: data.accountEnabled === false ? new Date().toISOString() : null }
       })
+
+      // Update identity provider if needed
+      if (data.accountEnabled != null) {
+        await this.identityProviderService.updateUserAsync(user.identityId, { accountEnabled: data.accountEnabled !== false })
+      }
+
+      // Remove cache entry
+      await this.cache.delete(user.identityId)
+    })
   }
 
 }
