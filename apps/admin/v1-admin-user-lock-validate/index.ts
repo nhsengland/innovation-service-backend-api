dd03c5d13b6e73eda39420ada7669cb0e9a65fb7
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2022-12-27 17:04:25 +0000

    endpoint for user lock validation

---
+++ apps/admin/v1-admin-user-lock-validate/index.ts
@@ -0,0 +1,84 @@
+import { mapOpenApi3 as openApi } from '@aaronpowell/azure-functions-nodejs-openapi';
+import type { AzureFunction, HttpRequest } from '@azure/functions';
+
+import { JwtDecoder } from '@admin/shared/decorators';
+import { JoiHelper, ResponseHelper, SwaggerHelper } from '@admin/shared/helpers';
+import {
+  AuthorizationServiceSymbol,
+  AuthorizationServiceType,
+} from '@admin/shared/services';
+import { UsersServiceSymbol, UsersServiceType } from '../_services/interfaces';
+import type { CustomContextType } from '@admin/shared/types';
+
+import { container } from '../_config';
+
+import { ParamsSchema, ParamsType } from './validation.schemas';
+import type { ResponseDTO } from './transformation.dtos';
+
+class V1AdminUserLockValidate {
+  @JwtDecoder()
+  static async httpTrigger(context: CustomContextType, request: HttpRequest): Promise<void> {
+    const authorizationService = container.get<AuthorizationServiceType>(
+      AuthorizationServiceSymbol
+    );
+
+    const usersService = container.get<UsersServiceType>(UsersServiceSymbol);
+
+    try {
+
+      const params = JoiHelper.Validate<ParamsType>(ParamsSchema, request.params);
+
+      await authorizationService
+        .validate(context.auth.user.identityId)
+        .checkAdminType()
+        .verify();
+
+      const result = await usersService.validateLockUser(params.userId);
+
+      context.res = ResponseHelper.Ok<ResponseDTO>({ validations: result });
+      return;
+    } catch (error) {
+      context.res = ResponseHelper.Error(context, error);
+      return;
+    }
+  }
+}
+
+export default openApi(
+  V1AdminUserLockValidate.httpTrigger as AzureFunction,
+  '/v1/users',
+  {
+    post: {
+      description: 'Create a user.',
+      operationId: 'v1-admin-user-create',
+      parameters: SwaggerHelper.paramJ2S({ path: ParamsSchema }),
+      responses: {
+        '200': {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  validations: {
+                    type: 'object',
+                    description: 'All the validations.',
+                  },
+                },
+              },
+            },
+          },
+        },
+        '400': {
+          description: 'Bad request.',
+        },
+        '401': {
+          description: 'The user is not authorized to access validation data.',
+        },
+        '500': {
+          description: 'An error occurred while fetching the validation data.',
+        },
+      },
+    },
+  }
+);
