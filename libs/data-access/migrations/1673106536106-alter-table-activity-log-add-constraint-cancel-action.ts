47133b6f821eee3f00e07126939f45561fc8e0fe
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2023-01-02 17:51:11 +0000

    added migration for action status update to Cancelled

---
+++ libs/data-access/migrations/1673106536106-alter-table-activity-log-add-constraint-cancel-action.ts
@@ -0,0 +1,75 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm'
+
+
+export class alterTableActivityLogAddConstraintCancelAction1673106536106 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+
+    await queryRunner.query(`
+      ALTER TABLE "activity_log" DROP CONSTRAINT "CK_activity_log_activity"
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE "activity_log" ADD CONSTRAINT "CK_activity_log_activity" CHECK (activity IN (
+        'INNOVATION_CREATION',
+        'OWNERSHIP_TRANSFER',
+        'SHARING_PREFERENCES_UPDATE',
+        'SECTION_DRAFT_UPDATE',
+        'SECTION_SUBMISSION',
+        'INNOVATION_SUBMISSION',
+        'NEEDS_ASSESSMENT_START',
+        'NEEDS_ASSESSMENT_COMPLETED',
+        'NEEDS_ASSESSMENT_EDITED',
+        'ORGANISATION_SUGGESTION',
+        'SUPPORT_STATUS_UPDATE',
+        'COMMENT_CREATION',
+        'ACTION_CREATION',
+        'ACTION_STATUS_IN_REVIEW_UPDATE',
+        'ACTION_STATUS_DECLINED_UPDATE',
+        'ACTION_STATUS_COMPLETED_UPDATE',
+        'ACTION_STATUS_REQUESTED_UPDATE',
+        'ACTION_STATUS_CANCELLED_UPDATE',
+        'THREAD_CREATION',
+        'THREAD_MESSAGE_CREATION',
+        'NEEDS_ASSESSMENT_REASSESSMENT_REQUESTED',
+        'INNOVATION_PAUSE'
+      ))`
+    );
+
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+
+    await queryRunner.query(`
+      ALTER TABLE "activity_log" DROP CONSTRAINT "CK_activity_log_activity"
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE "activity_log" ADD CONSTRAINT "CK_activity_log_activity" CHECK (activity IN (
+        'INNOVATION_CREATION',
+        'OWNERSHIP_TRANSFER',
+        'SHARING_PREFERENCES_UPDATE',
+        'SECTION_DRAFT_UPDATE',
+        'SECTION_SUBMISSION',
+        'INNOVATION_SUBMISSION',
+        'NEEDS_ASSESSMENT_START',
+        'NEEDS_ASSESSMENT_COMPLETED',
+        'NEEDS_ASSESSMENT_EDITED',
+        'ORGANISATION_SUGGESTION',
+        'SUPPORT_STATUS_UPDATE',
+        'COMMENT_CREATION',
+        'ACTION_CREATION',
+        'ACTION_STATUS_IN_REVIEW_UPDATE',
+        'ACTION_STATUS_DECLINED_UPDATE',
+        'ACTION_STATUS_COMPLETED_UPDATE',
+        'ACTION_STATUS_CANCELLED_UPDATE',
+        'ACTION_STATUS_REQUESTED_UPDATE',
+        'THREAD_CREATION', 'THREAD_MESSAGE_CREATION',
+        'NEEDS_ASSESSMENT_REASSESSMENT_REQUESTED',
+        'INNOVATION_PAUSE'
+      ))`
+    );
+
+  }
+
+}
