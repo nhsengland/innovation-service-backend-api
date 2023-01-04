888fa4fff10871247b3f6ecbc825b39214ceec88
Manuel Sousa <manuel.sousa@bjss.com>
2023-01-04 14:44:33 +0000

    Merged PR 15241: migrate v1-notifications-delete

--- apps/users/_services/notifications.service.ts
+++ apps/users/_services/notifications.service.ts
@@ -3,6 +3,7 @@ import type { InnovationStatusEnum, NotificationContextDetailEnum, NotificationC
 import type { PaginationQueryParamsType } from '@users/shared/helpers';
 import type { DateISOType } from '@users/shared/types';
 import { injectable } from 'inversify';
+import type { EntityManager } from 'typeorm';
 import { BaseService } from './base.service';
 
 @injectable()
@@ -12,27 +13,41 @@ export class NotificationsService extends BaseService {
 
   /**
    * gets the user active notifications counters
-   * @param id the user id
+   * @param userId the user id
+   * @param entityManager optional entity manager to run the query (for transactions)
    * @returns the total
    */
-  public async getUserActiveNotificationsCounter(id: string): Promise<number> {
-    const total = await this.sqlConnection.createQueryBuilder(NotificationUserEntity, 'notificationUser')
+  public async getUserActiveNotificationsCounter(userId: string, entityManager?: EntityManager): Promise<number> {
+    const em = entityManager ?? this.sqlConnection.manager;
+
+    const total = await em.createQueryBuilder(NotificationUserEntity, 'notificationUser')
       .innerJoin('notificationUser.notification', 'notification')
       .innerJoin('notification.innovation', 'innovation')             // fixes #104377
-      .where('notificationUser.user = :id', { id })
+      .where('notificationUser.user = :id', { id: userId })
       .andWhere('notificationUser.readAt IS NULL')
       .getCount();
     return total;
   }
 
 
+  /**
+   * gets the user notifications
+   * @param userId the user id
+   * @param filters optional filters to apply
+   * - contextTypes: the context types to filter by
+   * - unreadOnly: if true, only returns the unread notifications
+   * @param pagination the pagination params
+   * @param entityManager optional entity manager to run the query (for transactions)
+   * @returns the total and the notifications
+   */
   public async getUserNotifications(
-    id: string,
+    userId: string,
     filters: {
       contextTypes: NotificationContextTypeEnum[];
       unreadOnly: boolean;
     },
-    pagination: PaginationQueryParamsType<'createdAt'>
+    pagination: PaginationQueryParamsType<'createdAt'>,
+    entityManager?: EntityManager
   ): Promise<{total: number, data: {
     id: string;
     innovation: { id: string; name: string, status: InnovationStatusEnum };
@@ -43,13 +58,15 @@ export class NotificationsService extends BaseService {
     readAt: DateISOType;
     params: Record<string, unknown>;
   }[]}> {
-    const query = this.sqlConnection.createQueryBuilder(NotificationUserEntity, 'user')
+    const em = entityManager ?? this.sqlConnection.manager;
+
+    const query = em.createQueryBuilder(NotificationUserEntity, 'user')
       .innerJoinAndSelect('user.notification', 'notification')
       .innerJoin('notification.innovation', 'innovation')
       .addSelect('innovation.id', 'innovation_id')
       .addSelect('innovation.status', 'innovation_status')
       .addSelect('innovation.name', 'innovation_name')
-      .where('user.user = :userId', { userId: id })
+      .where('user.user = :userId', { userId: userId })
 
     // optional filters
     if (filters.unreadOnly) {
@@ -92,5 +109,17 @@ export class NotificationsService extends BaseService {
         params: JSON.parse(n.notification.params)
       }))
     };
+  }
+
+  /**
+   * deletes a user notification
+   * @param userId the user id
+   * @param notificationId the notification id
+   * @param entityManager optional entity manager to run the query (for transactions)
+   */
+  async deleteUserNotification(userId: string, notificationId: string, entityManager?: EntityManager): Promise<void> {
+    const em = entityManager ?? this.sqlConnection.manager;
+
+    await em.update(NotificationUserEntity, { user: userId, notification: notificationId }, { deletedAt: (new Date()).toISOString() });
   }
 }
