cd9063afb32539753e4a013b6e4d6d903c6999b5
Manuel Sousa <manuel.sousa@bjss.com>
2023-01-05 21:30:02 +0000

    Merged PR 15272: Add new index idx_notification_user_organisation_unit_read

--- libs/data-access/migrations/1672931339905-alter_notification_user_add_column_org_unit.ts
+++ libs/data-access/migrations/1672931339905-alter_notification_user_add_column_org_unit.ts
@@ -1,4 +1,4 @@
-import type { MigrationInterface, QueryRunner } from 'typeorm'
+import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class alterNotificationUserAddColumnOrgUnit1672931339905 implements MigrationInterface {
 
@@ -18,9 +18,11 @@ export class alterNotificationUserAddColumnOrgUnit1672931339905 implements Migra
         LEFT JOIN organisation_unit orgunit on orgunit.id = orgunituser.organisation_unit_id
         `
       )
+      await queryRunner.query(`CREATE INDEX "idx_notification_user_organisation_unit_read" ON "notification_user" ("user_id", "organisation_unit_id", "read_at") WHERE deleted_at IS NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`DROP INDEX "idx_notification_user_organisation_unit_read" ON "notification_user"`);
       await queryRunner.query(
           `ALTER TABLE "notification_user" DROP COLUMN organisation_unit_id;`
         );
