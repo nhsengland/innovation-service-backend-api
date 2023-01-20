0df58d6a8bc71d463bb42a4e47bc96924aa8b775
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2023-01-20 15:45:01 +0000

    fix general counter

--- apps/users/_services/notifications.service.ts
+++ apps/users/_services/notifications.service.ts
@@ -23,12 +23,13 @@ export class NotificationsService extends BaseService {
     const query = em.createQueryBuilder(NotificationUserEntity, 'notificationUser')
       .innerJoin('notificationUser.notification', 'notification')
       .innerJoin('notification.innovation', 'innovation')             // fixes #104377
-      .innerJoin('notificationUser.organisationUnit', 'orgUnit')
       .where('notificationUser.user = :id', { id: userId })
       .andWhere('notificationUser.readAt IS NULL')
 
     if (organisationUnitId) {
-      query.andWhere('orgUnit.id = :organisationUnitId', { organisationUnitId })
+      query.innerJoin('notificationUser.organisationUnit', 'orgUnit')
+      .andWhere('orgUnit.id = :organisationUnitId', { organisationUnitId })
+      
     }
 
     const total = await query.getCount()
@@ -69,15 +70,15 @@ export class NotificationsService extends BaseService {
 
     const query = em.createQueryBuilder(NotificationUserEntity, 'user')
       .innerJoinAndSelect('user.notification', 'notification')
-      .innerJoinAndSelect('user.organisationUnit', 'unit')
       .innerJoin('notification.innovation', 'innovation')
       .addSelect('innovation.id', 'innovation_id')
       .addSelect('innovation.status', 'innovation_status')
       .addSelect('innovation.name', 'innovation_name')
-      .where('user.user = :userId', { userId: user.id })
+      .where('user.user_id = :userId', { userId: user.id })
     
     if (user.organisationUnitId) {
-      query.andWhere('unit.id = :orgUnitId', { orgUnitId: user.organisationUnitId })
+      query.innerJoinAndSelect('user.organisationUnit', 'unit')
+      .andWhere('unit.id = :orgUnitId', { orgUnitId: user.organisationUnitId })
     }
 
     // optional filters
