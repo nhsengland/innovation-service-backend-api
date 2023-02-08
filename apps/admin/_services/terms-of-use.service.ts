0039d14a9f6da993d9143b5d8e01980944a6bf74
Manuel Sousa <manuel.sousa@bjss.com>
2023-02-08 17:25:13 +0000

    Merged PR 16101: add terms of use list

--- apps/admin/_services/terms-of-use.service.ts
+++ apps/admin/_services/terms-of-use.service.ts
@@ -1,7 +1,9 @@
 import { TermsOfUseEntity } from '@admin/shared/entities';
 import type { TermsOfUseTypeEnum } from '@admin/shared/enums';
+import type { PaginationQueryParamsType } from '@admin/shared/helpers';
 import type { DateISOType } from '@admin/shared/types';
 import { injectable } from 'inversify';
+import type { EntityManager } from 'typeorm';
 import { BaseService } from './base.service';
 
 @injectable()
@@ -60,9 +62,59 @@ export class TermsOfUseService extends BaseService {
           updatedBy: requestUser.id,
           releasedAt: touPayload.releasedAt || null
         }
-      )
-    })
+      );
+    });
 
-    return { id: touId }
+    return { id: touId };
+  }
+
+  /**
+   * gets a list of terms of use according to the pagination and ordering parameters.
+   * @param pagination pagination and ordering parameters.
+   * @param entityManager optional entity manager
+   * @returns list of terms of use and total count.
+   */
+  async getTermsOfUseList(pagination: PaginationQueryParamsType<'createdAt'>, entityManager?: EntityManager): Promise<{
+    count: number;
+    data: {
+      id: string;
+      name: string;
+      touType: TermsOfUseTypeEnum;
+      summary: string;
+      releaseAt: DateISOType | null;
+      createdAt: DateISOType;
+    }[]
+  }> {
+    const em = entityManager || this.sqlConnection.manager;
+    
+    const query = em.createQueryBuilder(TermsOfUseEntity, 'tou');
+    
+    // Pagination and ordering.
+    query.skip(pagination.skip);
+    query.take(pagination.take);
+
+    for (const [key, order] of Object.entries(pagination.order)) {
+      let field: string;
+      switch (key) {
+        case 'createdAt': field = 'tou.createdAt'; break;
+        default:
+          field = 'tou.createdAt'; break;
+      }
+      query.addOrderBy(field, order);
+    }
+
+    const termsOfUseList = await query.getManyAndCount();
+
+    return {
+      count: termsOfUseList[1],
+      data: termsOfUseList[0].map(t => ({
+        id: t.id,
+        name: t.name,
+        touType: t.touType,
+        summary: t.summary,
+        releaseAt: t.releasedAt,
+        createdAt: t.createdAt
+      }))
+    };
   }
 }
