c98ff2a117ccc29896135f02bfe3bf94553a188e
Manuel Sousa <manuel.sousa@bjss.com>
2023-04-14 16:55:02 +0000

    Merged PR 19103: simplify DateHelper

--- libs/shared/helpers/dates.helper.ts
+++ libs/shared/helpers/dates.helper.ts
@@ -7,16 +7,11 @@ export class DatesHelper {
    * Returns the difference (in days) between 2 dates.
    */
   static dateDiffInDays(startDate: Date, endDate: Date): number {
-
-    const sDate = new Date(startDate);
-    const eDate = new Date(endDate);
-
-    return Math.floor((eDate.getTime() - sDate.getTime()) / 1000 / 60 / 60 / 24);
-
+    return Math.floor((endDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 / 24);
   }
 
   static isDateEqual(firstDate: Date, secondDate: Date): boolean {
-    return new Date(firstDate).getTime() === new Date(secondDate).getTime();
+    return firstDate.getTime() === secondDate.getTime();
   }
 
 }
