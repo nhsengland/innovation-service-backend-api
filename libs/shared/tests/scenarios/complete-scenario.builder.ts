1e5f3ae9a83014608095d1745e90a65405216a84
Miguel-Ferro-bjss <miguel.ferro@bjss.com>
2023-06-19 15:56:41 +0100

    tests for needs-assessment-assessor-update handler

--- libs/shared/tests/scenarios/complete-scenario.builder.ts
+++ libs/shared/tests/scenarios/complete-scenario.builder.ts
@@ -40,6 +40,11 @@ export class CompleteScenarioBuilder {
         .addRole(ServiceRoleEnum.ASSESSMENT, 'assessmentRole')
         .save();
 
+      const seanNeedsAssessor = await new UserBuilder(entityManager)
+        .setName('Paul Needs Assessor')
+        .addRole(ServiceRoleEnum.ASSESSMENT, 'assessmentRole')
+        .save();
+
       // Admin
       const allMighty = await new UserBuilder(entityManager)
         .setName('All Mighty')
@@ -399,6 +404,10 @@ export class CompleteScenarioBuilder {
           paulNeedsAssessor: {
             ...paulNeedsAssessor,
             roles: { assessmentRole: paulNeedsAssessor.roles['assessmentRole']! }
+          },
+          seanNeedsAssessor: {
+            ...seanNeedsAssessor,
+            roles: { assessmentRole: seanNeedsAssessor.roles['assessmentRole']! }
           },
           // Admins
           allMighty: {
