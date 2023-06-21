c970a2433b539bab4dab7265a630033710631f79
Diogo Costa <diogo.costa@bjss.com>
2023-06-21 16:09:08 +0100

    fix: changed the approach of the fix

--- apps/innovations/_services/innovation-sections.service.ts
+++ apps/innovations/_services/innovation-sections.service.ts
@@ -812,7 +812,7 @@ export class InnovationSectionsService extends BaseService {
 
     return {
       ...sectionData,
-      files: [], // TECHDEBT: This can be removed with the new IR version
+      files: undefined, // TECHDEBT: This can be removed with the new IR version
       ...(evidenceData && { evidences: evidenceData })
     };
   }
