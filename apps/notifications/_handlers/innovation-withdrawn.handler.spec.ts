acf6255c6132d086b597417dbac7b77ff77b12c2
Miguel Ferro <miguel.ferro@bjss.com>
2023-06-21 11:10:28 +0000

    Merged PR 23021: Tests for handlers: unit-inactivation-support-status-complete, innovation-withdrawn
    ![image.png](https://nhsidev.visualstudio.com/7f3e8d94-c48d-41cc-b5aa-0aee0596a809/_apis/git/repositories/cdd4dc1b-8bd4-432c-a626-589b87be6712/pullRequests/23021/attachments/image.png)
    ![image (2).png](https://nhsidev.visualstudio.com/7f3e8d94-c48d-41cc-b5aa-0aee0596a809/_apis/git/repositories/cdd4dc1b-8bd4-432c-a626-589b87be6712/pullRequests/23021/attachments/image%20%282%29.png)

---
+++ apps/notifications/_handlers/innovation-withdrawn.handler.spec.ts
@@ -0,0 +1,85 @@
+import { CompleteScenarioType, MocksHelper, TestsHelper } from "@notifications/shared/tests";
+import { InnovationWithdrawnHandler } from "./innovation-withdrawn.handler";
+import { RecipientsService } from "../_services/recipients.service";
+import { DTOsHelper } from "@notifications/shared/tests/helpers/dtos.helper";
+import { EmailTypeEnum } from "../_config";
+import { NotificationContextDetailEnum, NotificationContextTypeEnum } from "@notifications/shared/enums";
+
+describe('Notifications / _handlers / innovation-withdrawn handler suite', () => {
+  let testsHelper: TestsHelper;
+  let scenario: CompleteScenarioType;
+
+  let handler: InnovationWithdrawnHandler;
+
+  let innovation: CompleteScenarioType['users']['johnInnovator']['innovations']['johnInnovation'];
+  let innovationOwner: CompleteScenarioType['users']['johnInnovator'];
+
+  beforeAll(async () => {
+    testsHelper = await new TestsHelper().init();
+    scenario = testsHelper.getCompleteScenario();
+
+    innovation = scenario.users.johnInnovator.innovations.johnInnovation;
+    innovationOwner = scenario.users.johnInnovator;
+  });
+
+  beforeEach(async () => {
+
+    jest.spyOn(RecipientsService.prototype, 'usersBagToRecipients').mockResolvedValueOnce([
+      DTOsHelper.getRecipientUser(scenario.users.jamieMadroxAccessor, 'aiRole'),
+      DTOsHelper.getRecipientUser(scenario.users.jamieMadroxAccessor, 'healthAccessorRole')
+    ]);
+
+    handler = new InnovationWithdrawnHandler(
+      DTOsHelper.getUserRequestContext(innovationOwner, 'innovatorRole'),
+      {
+        innovation: {
+          id: innovation.id,
+          name: innovation.name,
+          affectedUsers: [
+            {
+              userId: scenario.users.jamieMadroxAccessor.id,
+              userType: scenario.users.jamieMadroxAccessor.roles.aiRole.role,
+              organisationId: scenario.users.jamieMadroxAccessor.roles.aiRole.organisation?.id,
+              organisationUnitId: scenario.users.jamieMadroxAccessor.roles.aiRole.organisationUnit?.id
+            },
+            {
+              userId: scenario.users.jamieMadroxAccessor.id,
+              userType: scenario.users.jamieMadroxAccessor.roles.healthAccessorRole.role,
+              organisationId: scenario.users.jamieMadroxAccessor.roles.healthAccessorRole.organisation?.id,
+              organisationUnitId: scenario.users.jamieMadroxAccessor.roles.healthAccessorRole.organisationUnit?.id
+            }
+          ]
+        }
+      },
+      MocksHelper.mockContext()
+    )
+
+    await handler.run();
+  })
+
+  it('Should send email to all affectedUsers without duplicate emails', () => {
+    expect(handler.emails).toMatchObject([{
+        templateId: EmailTypeEnum.INNOVATION_WITHDRAWN_TO_ASSIGNED_USERS,
+        notificationPreferenceType: 'SUPPORT',
+        to: DTOsHelper.getRecipientUser(scenario.users.jamieMadroxAccessor, 'healthAccessorRole'),
+        params: {
+          innovation_name: innovation.name 
+        }
+    }])
+  })
+
+  it('Should inApp to all affectedUsers roles', () => {
+    expect(handler.inApp).toMatchObject([{
+        innovationId: innovation.id,
+        context: {
+          type: NotificationContextTypeEnum.INNOVATION,
+          detail: NotificationContextDetailEnum.INNOVATION_WITHDRAWN,
+          id: innovation.id 
+        },
+        userRoleIds: [scenario.users.jamieMadroxAccessor.roles.aiRole.id, scenario.users.jamieMadroxAccessor.roles.healthAccessorRole.id],
+        params: {
+          innovationName: innovation.name 
+        }
+    }])
+  })
+})
