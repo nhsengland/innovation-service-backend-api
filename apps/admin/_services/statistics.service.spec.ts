fdef7e90adf7b51ba25b70536f7be42eecb105a9
Miguel Ferro <miguel.ferro@bjss.com>
2023-08-04 15:22:20 +0000

    Merged PR 24184: Admin statistics service tests
    ![image.png](https://dev.azure.com/NHSiDev/7f3e8d94-c48d-41cc-b5aa-0aee0596a809/_apis/git/repositories/cdd4dc1b-8bd4-432c-a626-589b87be6712/pullRequests/24184/attachments/image.png)

---
+++ apps/admin/_services/statistics.service.spec.ts
@@ -0,0 +1,71 @@
+
+import { TestsHelper } from '@admin/shared/tests';
+
+import { container } from '../_config';
+import SYMBOLS from './symbols';
+import type { EntityManager } from 'typeorm';
+import type { StatisticsService } from './statistics.service';
+import { InnovationSupportEntity, OrganisationUnitEntity } from '@admin/shared/entities';
+import { InnovationSupportStatusEnum } from '@admin/shared/enums';
+import { InnovationEntity } from '@admin/shared/entities';
+
+describe('Admin / _services / announcements service suite', () => {
+  let sut: StatisticsService;
+
+  const testsHelper = new TestsHelper();
+  const scenario = testsHelper.getCompleteScenario();
+
+  let em: EntityManager;
+
+  beforeAll(async () => {
+    sut = container.get<StatisticsService>(SYMBOLS.StatisticsService);
+
+    await testsHelper.init();
+  });
+
+  beforeEach(async () => {
+    em = await testsHelper.getQueryRunnerEntityManager();
+  });
+
+  afterEach(async () => {
+    await testsHelper.releaseQueryRunnerEntityManager();
+  });
+
+  describe('getOrganisationUnitInnovationCounters', () => {
+    it('should return the count of innovations in each support status for the given organisation unit', async () => {
+      const resultBefore = await sut.getOrganisationUnitInnovationCounters(scenario.organisations.healthOrg.organisationUnits.healthOrgUnit.id, undefined, em)
+
+      expect(resultBefore).toMatchObject({ ENGAGING: 4 })
+
+      await em.getRepository(InnovationSupportEntity).save({
+        status: InnovationSupportStatusEnum.ENGAGING,
+        innovation: InnovationEntity.new({ id: scenario.users.adamInnovator.innovations.adamInnovationEmpty.id }),
+        organisationUnit: OrganisationUnitEntity.new({ id: scenario.organisations.healthOrg.organisationUnits.healthOrgUnit.id })
+      })
+
+      const result = await sut.getOrganisationUnitInnovationCounters(scenario.organisations.healthOrg.organisationUnits.healthOrgUnit.id, undefined, em)
+
+      expect(result).toMatchObject({ ENGAGING: 5 })
+    })
+
+    it('should return the count of innovations in each ongoing support status for the given organisation unit', async () => {
+      await em.getRepository(InnovationSupportEntity).save({
+        status: InnovationSupportStatusEnum.UNASSIGNED,
+        innovation: InnovationEntity.new({ id: scenario.users.adamInnovator.innovations.adamInnovationEmpty.id }),
+        organisationUnit: OrganisationUnitEntity.new({ id: scenario.organisations.healthOrg.organisationUnits.healthOrgUnit.id })
+      })
+
+      await em.getRepository(InnovationSupportEntity).save({
+        status: InnovationSupportStatusEnum.FURTHER_INFO_REQUIRED,
+        innovation: InnovationEntity.new({ id: scenario.users.johnInnovator.innovations.johnInnovationEmpty.id }),
+        organisationUnit: OrganisationUnitEntity.new({ id: scenario.organisations.healthOrg.organisationUnits.healthOrgUnit.id })
+      })
+
+      const result = await sut.getOrganisationUnitInnovationCounters(scenario.organisations.healthOrg.organisationUnits.healthOrgUnit.id, true, em)
+
+      expect(result).toMatchObject({ ENGAGING: 4, FURTHER_INFO_REQUIRED: 1 })
+
+    })
+  });
+
+});
