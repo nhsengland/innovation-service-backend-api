2d1b4f6e96b1cadcb95ff758accaa63be638f8e4
Manuel Sousa <manuel.sousa@bjss.com>
2023-09-26 16:27:27 +0100

    fix thread message is missing created by

--- libs/data-access/migrations/1695741965177-create-view-innovation-task-descriptions.ts
+++ libs/data-access/migrations/1695741965177-create-view-innovation-task-descriptions.ts
@@ -1,6 +1,6 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class createViewInnovationTaskDescriptions1695381567882 implements MigrationInterface {
+export class createViewInnovationTaskDescriptions1695741965177 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
     CREATE OR ALTER VIEW [innovation_task_descriptions_view] AS
@@ -18,7 +18,7 @@ export class createViewInnovationTaskDescriptions1695381567882 implements Migrat
     INNER JOIN innovation_task_message tm ON t.id = tm.innovation_task_id
     INNER JOIN innovation_thread_message m ON tm.innovation_thread_message_id = m.id
     INNER JOIN user_role r ON m.author_user_role_id = r.id
-    LEFT JOIN [user] u ON m.created_by = u.id AND u.status != 'DELETED'
+    LEFT JOIN [user] u ON m.author_id = u.id AND u.status != 'DELETED'
     LEFT JOIN organisation_unit ou ON r.organisation_unit_id = ou.id
     WHERE t.deleted_at IS NULL and m.deleted_at IS NULL
   `);
