404c0be1f7438c645d1b3ffccd1b4706d6f7c485
Manuel Sousa <manuel.sousa@bjss.com>
2023-10-10 15:18:40 +0100

    initial work to allow file uploads for threads/messages

--- apps/innovations/v1-innovation-thread-create/validation.schemas.ts
+++ apps/innovations/v1-innovation-thread-create/validation.schemas.ts
@@ -5,11 +5,23 @@ import { TEXTAREA_LENGTH_LIMIT } from '@innovations/shared/constants';
 export type BodyType = {
   subject: string;
   message: string;
-  followerUserRoleIds: string[]
+  file?: {
+    id: string;
+    name: string;
+    size: number;
+    extension: string;
+  };
+  followerUserRoleIds: string[];
 };
 export const BodySchema = Joi.object<BodyType>({
   subject: Joi.string().max(200).required(),
   message: Joi.string().max(TEXTAREA_LENGTH_LIMIT.xxl).required(),
+  file: Joi.object({
+    id: Joi.string().max(100).required(),
+    name: Joi.string().max(100).required(),
+    size: Joi.number().required(),
+    extension: Joi.string().max(4).required()
+  }),
   followerUserRoleIds: Joi.array().items(Joi.string().guid()).required()
 }).required();
 
--- apps/innovations/v1-innovation-thread-message-create/validation.schemas.ts
+++ apps/innovations/v1-innovation-thread-message-create/validation.schemas.ts
@@ -4,9 +4,21 @@ import { TEXTAREA_LENGTH_LIMIT } from '@innovations/shared/constants';
 
 export type BodyType = {
   message: string;
+  file?: {
+    id: string;
+    name: string;
+    size: number;
+    extension: string;
+  };
 };
 export const BodySchema = Joi.object<BodyType>({
-  message: Joi.string().max(TEXTAREA_LENGTH_LIMIT.xxl).required()
+  message: Joi.string().max(TEXTAREA_LENGTH_LIMIT.xxl).required(),
+  file: Joi.object({
+    id: Joi.string().max(100).required(),
+    name: Joi.string().max(100).required(),
+    size: Joi.number().required(),
+    extension: Joi.string().max(4).required()
+  })
 }).required();
 
 export type ParamsType = {
