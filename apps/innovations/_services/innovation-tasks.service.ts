5a9f8a92febec48596ddeef8eb4f4d64d47a6fdc
Diogo Costa <diogo.costa@bjss.com>
2023-10-19 12:34:54 +0100

    fix: tests

--- apps/innovations/_services/innovation-tasks.service.ts
+++ apps/innovations/_services/innovation-tasks.service.ts
@@ -14,6 +14,7 @@ import {
   InnovationStatusEnum,
   InnovationSupportStatusEnum,
   InnovationTaskStatusEnum,
+  NotificationCategoryEnum,
   NotificationContextTypeEnum,
   NotifierTypeEnum,
   ServiceRoleEnum,
@@ -244,7 +245,7 @@ export class InnovationTasksService extends BaseService {
 
     let notifications: {
       id: string;
-      contextType: NotificationContextTypeEnum;
+      contextType: NotificationContextTypeEnum | NotificationCategoryEnum;
       contextId: string;
       params: Record<string, unknown>;
     }[] = [];
