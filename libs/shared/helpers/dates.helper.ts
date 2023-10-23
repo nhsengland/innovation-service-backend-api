a41e57ff7c752ca558e0eba01a526dd4071406f5
Manuel Sousa <manuel.sousa@bjss.com>
2023-10-23 12:47:00 +0100

    add addWorkingDays helper

--- libs/shared/helpers/dates.helper.ts
+++ libs/shared/helpers/dates.helper.ts
@@ -9,4 +9,26 @@ export class DatesHelper {
   static isDateEqual(firstDate: Date, secondDate: Date): boolean {
     return firstDate.getTime() === secondDate.getTime();
   }
+
+  /**
+   * Adds the specified number of working days to the given date.
+   * @param date The date to add working days to.
+   * @param days The number of working days to add.
+   * @returns A new Date object representing the resulting date.
+   */
+  static addWorkingDays(date: Date, days: number): Date {
+    const result = new Date(date);
+
+    // Add the specified number of working days
+    while (days > 0) {
+      result.setDate(result.getDate() + 1);
+
+      // Skip weekends (Saturday and Sunday)
+      if (result.getDay() !== 0 && result.getDay() !== 6) {
+        days--;
+      }
+    }
+
+    return result;
+  }
 }
