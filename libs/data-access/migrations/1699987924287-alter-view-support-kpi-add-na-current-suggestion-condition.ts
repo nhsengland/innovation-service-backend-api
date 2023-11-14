c42c382b1f058ae1217a537b0032d2c5cc26de34
Manuel Sousa <manuel.sousa@bjss.com>
2023-11-14 19:19:07 +0000

    ...

--- libs/data-access/migrations/1699987924287-alter-view-support-kpi-add-na-current-suggestion-condition.ts
+++ libs/data-access/migrations/1699987924287-alter-view-support-kpi-add-na-current-suggestion-condition.ts
@@ -2,7 +2,7 @@ import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 // This view is used to calculate the KPI for the support team, this can be improved once we don't need to query the
 // activity log
-export class createViewSupportKPI1698162171281 implements MigrationInterface {
+export class alterViewSupportKPIAddNACurrentSuggestedCondition1699987924287 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
     CREATE OR ALTER VIEW [innovation_support_kpi_view] AS
