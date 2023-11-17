7c47874160e278eed8dfb7b00d3a7474d2ce0a5f
Diogo Costa <diogo.costa@bjss.com>
2023-11-17 13:41:12 +0000

    fix: missplaced ponctuation

--- libs/data-access/migrations/1700222729416-soft-delete-all-notifications.ts
+++ libs/data-access/migrations/1700222729416-soft-delete-all-notifications.ts
@@ -2,15 +2,8 @@ import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class softDeleteAllNotifications1700222729416 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-      UPDATE notification_user
-      SET deleted_at = GETDATE()
-      WHERE deleted_at IS NULL;
-
-      UPDATE notification
-      SET deleted_at = GETDATE();
-      WHERE deleted_at IS NULL;
-    `);
+    await queryRunner.query(`UPDATE notification_user SET deleted_at = GETDATE() WHERE deleted_at IS NULL;`);
+    await queryRunner.query(`UPDATE notification SET deleted_at = GETDATE() WHERE deleted_at IS NULL;`);
   }
 
   public async down(): Promise<void> {}
