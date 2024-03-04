2b7e17aeab3bddfc3cab3e9eca7ca7a6b988eb90
pablocodeglia <pablo.codeglia@bjss.com>
2024-03-04 13:34:22 +0000

    renamed migration

--- libs/data-access/migrations/1709559213411-alter-table-user-add-how_did_you_find_us_answers.ts
+++ libs/data-access/migrations/1709559213411-alter-table-user-add-how_did_you_find_us_answers.ts
@@ -1,6 +1,6 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class alterTableUserAddHowDidYouFindUs1709323839299 implements MigrationInterface {
+export class alterTableUserAddHowDidYouFindUs1709559213411 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "user" ADD how_did_you_find_us_answers nvarchar(MAX);`);
   }
