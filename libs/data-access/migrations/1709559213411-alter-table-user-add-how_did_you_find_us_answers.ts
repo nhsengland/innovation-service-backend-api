55151f29f7aa0d79d1a94cb659f8f97f98e5a9ae
pablocodeglia <pablo.codeglia@bjss.com>
2024-03-05 09:34:00 +0000

    Fixes migration script

--- libs/data-access/migrations/1709559213411-alter-table-user-add-how_did_you_find_us_answers.ts
+++ libs/data-access/migrations/1709559213411-alter-table-user-add-how_did_you_find_us_answers.ts
@@ -3,7 +3,7 @@ import type { MigrationInterface, QueryRunner } from 'typeorm';
 export class alterTableUserAddHowDidYouFindUs1709559213411 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "user" ADD how_did_you_find_us_answers nvarchar(MAX) CONSTRAINT "CK_user_how_did_you_find_us_answers_is_json" CHECK (ISJSON(params)=1);`
+      `ALTER TABLE "user" ADD how_did_you_find_us_answers nvarchar(MAX) CONSTRAINT "CK_user_how_did_you_find_us_answers_is_json" CHECK (ISJSON(how_did_you_find_us_answers)=1);`
     );
   }
 
