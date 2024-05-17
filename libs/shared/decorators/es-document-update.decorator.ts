37c37403596b1c6abe2cd71ac0506cf756ccae99
Diogo M. Costa <diogo.costa@bjss.com>
2024-05-17 19:12:49 +0100

    feat: decorator to trigger innovation index update
    Decorator that will be used on the endpoints that will trigger the
    innovation update on the elastic search index. It was created to make it
    easy and reusable to update the index (when a innovation update occurs).

---
+++ libs/shared/decorators/es-document-update.decorator.ts
@@ -0,0 +1,44 @@
+import type { HttpRequest } from '@azure/functions';
+import { container } from '../config/inversify.config';
+import type { StorageQueueService } from '../services';
+import { QueuesEnum } from '../services/integrations/storage-queue.service';
+import SHARED_SYMBOLS from '../services/symbols';
+import type { CustomContextType } from '../types';
+
+export const ElasticSearchEventUpdateTypes = [
+  'INNOVATION_UPDATE',
+  'ASSESSMENT_UPDATE',
+  'SUPPORT_UPDATE',
+  'SUGGESTION_UPDATE'
+] as const;
+export type ElasticSearchEventUpdateTypes = (typeof ElasticSearchEventUpdateTypes)[number];
+
+export type ElasticSearchEventUpdateMessageType = {
+  data: {
+    innovationId: string;
+    type: ElasticSearchEventUpdateTypes;
+  };
+};
+
+export function ElasticSearchDocumentUpdate(type: ElasticSearchEventUpdateTypes) {
+  return function (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
+    const original = descriptor.value;
+
+    const storageService = container.get<StorageQueueService>(SHARED_SYMBOLS.StorageQueueService);
+
+    descriptor.value = async function (context: CustomContextType, request: HttpRequest) {
+      await original.apply(this, [context, request]);
+
+      if (context.res?.['status'] >= 400) {
+        return;
+      }
+
+      const innovationId =
+        request.params['innovationId'] ?? request.query['innovationId'] ?? request.body['innovationId'];
+
+      await storageService.sendMessage<ElasticSearchEventUpdateMessageType>(QueuesEnum.ELASTIC_SEARCH, {
+        data: { innovationId, type }
+      });
+    };
+  };
+}
