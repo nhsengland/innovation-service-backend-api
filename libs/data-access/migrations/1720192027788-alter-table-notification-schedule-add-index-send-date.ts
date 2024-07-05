fcbea445741705cedbab6ed65e24dda8c2faa14a
Manuel Sousa <manuel.sousa@bjss.com>
2024-07-05 18:09:12 +0100

    sql changes

---
+++ libs/data-access/migrations/1720192027788-alter-table-notification-schedule-add-index-send-date.ts
@@ -0,0 +1,16 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class addIndexNotifyMe1719310027344 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE "notification_schedule" DROP CONSTRAINT "fk_notification_schedule_user_role_id";
+      ALTER TABLE "notification_schedule" DROP CONSTRAINT "df_notification_schedule_params";
+      ALTER TABLE "notification_schedule" DROP CONSTRAINT "ck_notification_schedule_params_is_json";
+      ALTER TABLE "notification_schedule" DROP COLUMN "user_role_id";
+      ALTER TABLE "notification_schedule" DROP COLUMN "params";
+      CREATE INDEX "idx_notification_schedule_send_date" ON "notification_schedule" ("send_date");
+    `);
+  }
+
+  async down(): Promise<void> {}
+}
