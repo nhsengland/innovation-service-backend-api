37ce9a0000754ad5261dcad7ea0243182e884b7f
Diogo M. Costa <diogo.costa@bjss.com>
2024-08-21 16:26:44 +0100

    task: add migration to change legacy announcements

--- libs/shared/enums/announcement.enums.ts
+++ libs/shared/enums/announcement.enums.ts
@@ -1,21 +1,7 @@
-export const AnnouncementTemplateType = ['GENERIC', 'FILTERED', 'DEPRECATED'] as const;
-export type AnnouncementTemplateType = (typeof AnnouncementTemplateType)[number];
-
 export type AnnouncementParamsType = {
-  GENERIC: {
-    content: string;
-    link?: { label: string; url: string };
-  };
-  FILTERED: {
-    content: string;
-    link?: { label: string; url: string };
-    filters: any; // TODO: put the right type afterwards.
-  };
-  DEPRECATED: {
-    inset?: { title?: string; content?: string; link?: { label: string; url: string } };
-    content?: string;
-    actionLink?: { label: string; url: string };
-  };
+  content: string;
+  link?: { label: string; url: string };
+  filters?: any; // TODO: put the right type afterwards.
 };
 
 export enum AnnouncementStatusEnum {
