3db1d68fb1ae0d33d4becae266bdecceeb09bb4e
Pedro Carloto <pedro.carloto@bjss.com>
2024-08-21 17:02:34 +0100

    fix: changed import to type

--- libs/shared/entities/user/announcement.entity.ts
+++ libs/shared/entities/user/announcement.entity.ts
@@ -6,7 +6,7 @@ import type { ServiceRoleEnum } from '../../enums/user.enums';
 import { AnnouncementTemplateType, AnnouncementTypeEnum } from '../../enums/announcement.enums';
 
 import { AnnouncementUserEntity } from './announcement-user.entity';
-import { FilterPayload } from '../../models/schema-engine/schema.model';
+import type { FilterPayload } from '../../models/schema-engine/schema.model';
 
 @Entity('announcement')
 export class AnnouncementEntity extends BaseEntity {
