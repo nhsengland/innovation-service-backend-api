0f8ce55e9c6f83bb6401b24196263bc0d80be492
Pedro Carloto <pedro.carloto@bjss.com>
2024-09-20 11:10:47 +0100

    fix: fixed typo and removed distinct

--- libs/data-access/migrations/1726826937932-create-view-innovation-relevant-organisation-status.ts
+++ libs/data-access/migrations/1726826937932-create-view-innovation-relevant-organisation-status.ts
@@ -6,7 +6,7 @@ import type { MigrationInterface, QueryRunner } from 'typeorm';
 // - SUGGESTED
 // - PREVIOUS_ENGAGED
 //
-export class createViewInnovationRelevantOrganisationsStatus1726668369380 implements MigrationInterface {
+export class createViewInnovationRelevantOrganisationsStatus1726826937932 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
     CREATE OR ALTER VIEW [innovation_relevant_organisations_status_view] AS
@@ -15,7 +15,7 @@ export class createViewInnovationRelevantOrganisationsStatus1726668369380 implem
   FROM innovation_support s
   WHERE s.status IN ('ENGAGING','WAITING')
 ), previous_engaged AS (
-  SELECT s.innovation_id, s.organisation_unit_id, 'PREVIOUS_ENGANGED' as status, null as support_id
+  SELECT s.innovation_id, s.organisation_unit_id, 'PREVIOUS_ENGAGED' as status, null as support_id
   FROM innovation_support FOR SYSTEM_TIME ALL s
   WHERE s.status = 'ENGAGING'
   AND s.organisation_unit_id NOT IN (SELECT organisation_unit_id FROM engaging_waiting WHERE innovation_id = s.innovation_id)
@@ -43,9 +43,9 @@ JSON_OBJECT('id': ou.id, 'name': ou.name, 'acronym': ou.acronym) AS organisation
 IIF(
   s.support_id IS NULL,
   -- if not support I want all the QAs
-  (SELECT DISTINCT id as roleId, user_id as userId FROM user_role WHERE organisation_unit_id = ou.id AND role='QUALIFYING_ACCESSOR' AND is_active = 1 FOR JSON AUTO),
+  (SELECT id as roleId, user_id as userId FROM user_role WHERE organisation_unit_id = ou.id AND role='QUALIFYING_ACCESSOR' AND is_active = 1 FOR JSON AUTO),
   -- if support I want the assigned
-  (SELECT DISTINCT id as roleId, user_id as userId FROM innovation_support_user su
+  (SELECT id as roleId, user_id as userId FROM innovation_support_user su
    INNER JOIN user_role r ON su.user_role_id=r.id AND r.is_active = 1 WHERE su.innovation_support_id = s.support_id FOR JSON AUTO)
 ) as user_data
 FROM all_supports s
