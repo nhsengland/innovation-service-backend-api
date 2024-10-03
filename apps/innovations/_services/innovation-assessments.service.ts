fa9fe765ee6359a05290dac2c59b1d4cccc0f1d2
Diogo M. Costa <diogo.costa@bjss.com>
2024-10-03 17:56:40 +0100

    task(AB#176623): review reassessment flow

--- apps/innovations/_services/innovation-assessments.service.ts
+++ apps/innovations/_services/innovation-assessments.service.ts
@@ -635,7 +635,6 @@ export class InnovationAssessmentsService extends BaseService {
     }
 
     const result = await connection.transaction(async transaction => {
-      // 0. Restore old supports if they exist and innovation is ARCHIVED
       // 1. Update the innovation status to WAITING_NEEDS_ASSESSMENT
       // 2. Sync the changes on the documents (update submitted with the draft changes)
       // 3. Soft deletes the previous assessment record
@@ -645,39 +644,6 @@ export class InnovationAssessmentsService extends BaseService {
       // 7. Sends notifications
 
       const now = new Date();
-
-      if (
-        innovation &&
-        innovation.status === InnovationStatusEnum.ARCHIVED &&
-        innovation.innovationSupports.length > 0
-      ) {
-        if (1 < Number(5))
-          throw new Error(
-            'TODO MJS review this archive logic, keeping old code commented but likely this can be removed'
-          );
-        /*
-        await transaction.save(
-          InnovationSupportEntity,
-          innovation.innovationSupports
-            .filter(support => support.archiveSnapshot !== null)
-            .map(support => {
-              const snapshot = support.archiveSnapshot;
-              if (snapshot) {
-                support.status =
-                  snapshot.status !== InnovationSupportStatusEnum.ENGAGING
-                    ? snapshot.status
-                    : InnovationSupportStatusEnum.SUGGESTED; // TODO MJS This will be reviewed later
-                support.userRoles =
-                  snapshot.status === InnovationSupportStatusEnum.WAITING
-                    ? snapshot.assignedAccessors.map(id => UserRoleEntity.new({ id }))
-                    : [];
-              }
-              support.archiveSnapshot = null;
-              return support;
-            })
-        );
-        */
-      }
 
       await this.documentService.syncDocumentVersions(domainContext, innovationId, transaction, { updatedAt: now });
 
