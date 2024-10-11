674df5fd9883b3f931a1a08677d1ef7fe236a145
Manuel Sousa <manuel.sousa@bjss.com>
2024-10-11 10:51:31 +0100

    update innovationsToReview

---
+++ libs/data-access/migrations/1728570506531-drop-view-innovation-support-kpi.ts
@@ -0,0 +1,11 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+// This view is used to calculate the KPI for the support team, this can be improved once we don't need to query the
+// activity log
+export class dropViewInnovationSupportKPI1728570506531 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP VIEW [innovation_support_kpi_view]`);
+  }
+
+  async down(): Promise<void> {}
+}
