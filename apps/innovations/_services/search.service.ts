28a4d65a3b1e20851f4bd9eb032fb7ac6605353e
Manuel Sousa <manuel.sousa@bjss.com>
2024-10-25 11:39:52 +0100

    ...

--- apps/innovations/_services/search.service.ts
+++ apps/innovations/_services/search.service.ts
@@ -420,7 +420,7 @@ export class SearchService extends BaseService {
           type: 'best_fields',
           query: search,
           fields: [...priorities, 'document.*'],
-          fuzziness: 'AUTO',
+          fuzziness: 0,
           prefix_length: 2,
           tie_breaker: 0.3
           // minimum_should_match: '2<-25% 9<-3'
