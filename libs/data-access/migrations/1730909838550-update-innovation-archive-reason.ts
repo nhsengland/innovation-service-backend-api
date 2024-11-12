16928b3b886af500b299f9904e70720fc86ae5f9
Manuel Sousa <manuel.sousa@bjss.com>
2024-11-12 10:42:29 +0000

    drop meta column archived_status

--- libs/data-access/migrations/1730909838550-update-innovation-archive-reason.ts
+++ libs/data-access/migrations/1730909838550-update-innovation-archive-reason.ts
@@ -2,6 +2,7 @@ import { type MigrationInterface, type QueryRunner } from 'typeorm';
 
 export class UpdateInnovationArchiveReason1730909838550 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE innovation DROP COLUMN archived_status');
     await queryRunner.query(`UPDATE innovation SET archive_reason = 'LEGACY' WHERE status = 'ARCHIVED'`);
   }
   async down(_queryRunner: QueryRunner): Promise<void> {}
