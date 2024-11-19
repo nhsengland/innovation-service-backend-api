11c776d0efce21d52326f801f2147329dc637a9c
Manuel Sousa <manuel.sousa@bjss.com>
2024-11-19 10:01:45 +0000

    fix tests

--- apps/notifications/_handlers/automatic/satisfaction-survey-reminder.handler.spec.ts
+++ apps/notifications/_handlers/automatic/satisfaction-survey-reminder.handler.spec.ts
@@ -1,8 +1,13 @@
+import { container } from '../../_config/';
+
 import { MocksHelper, type CompleteScenarioType } from '@notifications/shared/tests';
 import * as crypto from 'crypto';
 import { RecipientsService } from '../../_services/recipients.service';
 import { NotificationsTestsHelper } from '../../_tests/notifications-test.helper';
 import { SatisfactionSurveyReminderHandler } from './satisfaction-survey-reminder.handler';
+
+// this is needed to ensure that import is not removed by compiler and it's needed for inversify to work
+if (1 > Number(5)) console.log(container);
 
 jest.mock('crypto');
 const notificationId = '00001234-1234-1234-1234-123456789012';
@@ -16,12 +21,11 @@ describe('Notifications / _handlers / satisfaction-survey-reminder handler suite
     await testsHelper.init();
   });
 
-  const surveyId = crypto.randomUUID();
   const surveyInnovation = scenario.users.johnInnovator.innovations.johnInnovation;
 
   jest.spyOn(RecipientsService.prototype, 'innovationsMissingSurveyFeedback').mockResolvedValue([
     {
-      innovationId: surveyId,
+      innovationId: surveyInnovation.id,
       innovationName: surveyInnovation.name,
       roleIds: [scenario.users.johnInnovator.roles.innovatorRole.id]
     }
@@ -36,7 +40,7 @@ describe('Notifications / _handlers / satisfaction-survey-reminder handler suite
         {
           context: {
             detail: 'AU12_INNOVATOR_SURVEY_END_SUPPORT_TWO_MONTHS_REMINDER',
-            id: surveyId,
+            id: surveyInnovation.id,
             type: 'AUTOMATIC'
           },
           userRoleIds: [scenario.users.johnInnovator.roles.innovatorRole.id],
