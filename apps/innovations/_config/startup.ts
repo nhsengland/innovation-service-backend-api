1328f2989368f92578687bc3ebd63cccecf9939a
Manuel Sousa <manuel.sousa@bjss.com>
2024-12-03 13:39:18 +0000

    ...

--- apps/innovations/_config/startup.ts
+++ apps/innovations/_config/startup.ts
@@ -13,24 +13,21 @@ export const startup = async (container: Container): Promise<void> => {
   logger.log('Initializing Innovations app function');
 
   /* c8 ignore next 17 */
-  // TOOD remove
-  if (Number(1) > 5) {
-    if (process.env['LOCAL_MODE'] ?? false) {
-      try {
-        console.group('Generating documentation...');
+  if (process.env['LOCAL_MODE'] ?? false) {
+    try {
+      console.group('Generating documentation...');
 
-        const response = await httpService.getHttpInstance().get(`http://127.0.0.1:7072/api/swagger.json`);
-        console.log('Saving swagger file');
-        fs.writeFileSync(
-          `${join(__dirname, '../../../..')}/apps/innovations/.apim/swagger.yaml`,
-          YAML.stringify(response.data)
-        );
-        console.log('Documentation generated successfully');
-        console.groupEnd();
-      } catch (error) {
-        // TODO: Treat this error! Should we end the process?
-        logger.error('Innovations app function was UNABLE to start', { error });
-      }
+      const response = await httpService.getHttpInstance().get(`http://127.0.0.1:7072/api/swagger.json`);
+      console.log('Saving swagger file');
+      fs.writeFileSync(
+        `${join(__dirname, '../../../..')}/apps/innovations/.apim/swagger.yaml`,
+        YAML.stringify(response.data)
+      );
+      console.log('Documentation generated successfully');
+      console.groupEnd();
+    } catch (error) {
+      // TODO: Treat this error! Should we end the process?
+      logger.error('Innovations app function was UNABLE to start', { error });
     }
   }
 
