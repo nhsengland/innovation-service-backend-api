926f6eea5121522608853739cf7daf28ee2271bf
Manuel Sousa <manuel.sousa@bjss.com>
2024-12-03 18:16:09 +0000

    ...

--- libs/data-access/migrations/1733243128510-add-system-user-db.ts
+++ libs/data-access/migrations/1733243128510-add-system-user-db.ts
@@ -4,16 +4,16 @@ export class AddSystemUser1733243128510 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `
+      -- Seed has this but environments don't ... this is strange
+      IF NOT EXISTS (SELECT 1 FROM [user] WHERE id = '00000000-0000-0000-0000-000000000000')
+      BEGIN
       INSERT INTO [user] (created_at, created_by, updated_at, updated_by, id, external_id, status) 
       VALUES (GETDATE(), '00000000-0000-0000-0000-000000000000', GETDATE(), '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'DELETED');
+      END
+
       INSERT INTO user_role (created_at, created_by, updated_at, updated_by, id, role, user_id, is_active) 
       VALUES (GETDATE(), '00000000-0000-0000-0000-000000000000', GETDATE(), '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'ADMIN', '00000000-0000-0000-0000-000000000000', 0);`
     );
   }
-  async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-      DELETE FROM [user_role] where user_id='00000000-0000-0000-0000-000000000000';
-      DELETE FROM [user] where id='00000000-0000-0000-0000-000000000000';
-    `);
-  }
+  async down(): Promise<void> {}
 }
