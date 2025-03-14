29ad8ebe7ac371359ddf0c27f2477b0629bad30e
Pedro Carloto <pedro.carloto@bjss.com>
2025-03-14 16:34:30 +0000

    fix: changed import

--- apps/users/_handlers/statistics/innovations-needing-action.handler.ts
+++ apps/users/_handlers/statistics/innovations-needing-action.handler.ts
@@ -1,10 +1,10 @@
-import type { DomainContextType } from '@innovations/shared/types';
 import { container } from '../../_config';
 
 import type { StatisticsService } from '../../_services/statistics.service';
 import SYMBOLS from '../../_services/symbols';
 import type { UserStatisticsEnum } from 'apps/users/_enums/user.enums';
 import type { UserStatisticsTemplateType } from 'apps/users/_config/statistics.config';
+import type { DomainContextType } from '@users/shared/types';
 
 export const InnovationsNeedingActionStatisticsHandler = async (
   domainContext: DomainContextType
