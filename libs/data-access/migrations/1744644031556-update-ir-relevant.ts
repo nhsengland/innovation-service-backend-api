b321d20830c8fa46cbbba12c2e37830fea693174
Manuel Sousa <manuel.sousa@bjss.com>
2025-04-14 17:29:48 +0100

    wip

--- libs/data-access/migrations/1744644031556-update-ir-relevant.ts
+++ libs/data-access/migrations/1744644031556-update-ir-relevant.ts
@@ -3,11 +3,63 @@ import { IR_SCHEMA } from '../../shared/schemas/innovation-record/schema';
 
 export class updateIRRelevant1744644031556 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const translations = {
-      COVID_19: 'EMERGING_INFECTIOUS_DISEASES',
-      INDEPENDENCE_AND_PREVENTION: 'PREVENTIVE_CARE',
-      PATIENT_ACTIVATION_AND_SELF_CARE: 'SUPPORTING_PEOPLE_HEALTH'
-    };
+    await queryRunner.query(
+      `
+      UPDATE innovation_document
+      SET document = JSON_MODIFY(
+        document,
+        '$.INNOVATION_DESCRIPTION.areas',
+        JSON_QUERY(
+		    REPLACE(
+        REPLACE(
+        REPLACE(
+        REPLACE(
+          REPLACE(
+            REPLACE(
+              REPLACE(
+                JSON_QUERY(document, '$.INNOVATION_DESCRIPTION.areas'),
+                'COVID_19', 'EMERGING_INFECTIOUS_DISEASES'
+              ),
+              'INDEPENDENCE_AND_PREVENTION', 'PREVENTIVE_CARE'
+            ),
+            'PATIENT_ACTIVATION_AND_SELF_CARE', 'SUPPORTING_PEOPLE_HEALTH'
+          ),
+		  '"OPERATIONAL_EXCELLENCE"', ''),
+		  '[,', '['),
+		  ',]', ']'),
+		  ',,', ',')
+        )
+      )
+      WHERE JSON_QUERY(document, '$.INNOVATION_DESCRIPTION.areas') IS NOT NULL;
+
+      UPDATE innovation_document_draft
+      SET document = JSON_MODIFY(
+        document,
+        '$.INNOVATION_DESCRIPTION.areas',
+        JSON_QUERY(
+		    REPLACE(
+        REPLACE(
+        REPLACE(
+        REPLACE(
+          REPLACE(
+            REPLACE(
+              REPLACE(
+                JSON_QUERY(document, '$.INNOVATION_DESCRIPTION.areas'),
+                'COVID_19', 'EMERGING_INFECTIOUS_DISEASES'
+              ),
+              'INDEPENDENCE_AND_PREVENTION', 'PREVENTIVE_CARE'
+            ),
+            'PATIENT_ACTIVATION_AND_SELF_CARE', 'SUPPORTING_PEOPLE_HEALTH'
+          ),
+		  '"OPERATIONAL_EXCELLENCE"', ''),
+		  '[,', '['),
+		  ',]', ']'),
+		  ',,', ',')
+        )
+      )
+      WHERE JSON_QUERY(document, '$.INNOVATION_DESCRIPTION.areas') IS NOT NULL;
+      `
+    );
 
     await queryRunner.query(
       `
